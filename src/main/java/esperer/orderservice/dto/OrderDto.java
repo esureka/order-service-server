673b3169dbfcedeae499c13f0a0e97080dce913c
aesperer <piyrw9754@gmail.com>
2023-07-27 20:21:20 +0900

    order dto entity

---
+++ src/main/java/esperer/orderservice/dto/OrderDto.java
@@ -0,0 +1,19 @@
+package esperer.orderservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class OrderDto implements Serializable {
+    private String productId;
+    private Integer qty;
+    private Integer unitPrice;
+    private Integer totalPrice;
+    private Date createAt;
+
+    private String orderId;
+}
